77e621519798a60a4ec66f6bf1af7f3ce503da90
David Leiton <jdavid1506@gmail.com>
2017-06-19 12:52:16 -0600

    Interfaz y validaciones

---
+++ Subneteo/Subneteo/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Subneteo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.slashBox = new System.Windows.Forms.ComboBox();
+            this.Mascara = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.requiredBox = new System.Windows.Forms.ComboBox();
+            this.requiredNumberInput = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.rangosButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.oct1Input = new System.Windows.Forms.NumericUpDown();
+            this.oct4Input = new System.Windows.Forms.NumericUpDown();
+            this.oct3Input = new System.Windows.Forms.NumericUpDown();
+            this.oct2Input = new System.Windows.Forms.NumericUpDown();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.requiredNumberInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oct1Input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oct4Input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oct3Input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oct2Input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // slashBox
+            // 
+            this.slashBox.FormattingEnabled = true;
+            this.slashBox.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30"});
+            this.slashBox.Location = new System.Drawing.Point(206, 117);
+            this.slashBox.Name = "slashBox";
+            this.slashBox.Size = new System.Drawing.Size(64, 21);
+            this.slashBox.TabIndex = 1;
+            this.slashBox.SelectedIndexChanged += new System.EventHandler(this.claseBox_SelectedIndexChanged);
+            // 
+            // Mascara
+            // 
+            this.Mascara.AutoSize = true;
+            this.Mascara.Location = new System.Drawing.Point(24, 120);
+            this.Mascara.Name = "Mascara";
+            this.Mascara.Size = new System.Drawing.Size(133, 13);
+            this.Mascara.TabIndex = 1;
+            this.Mascara.Text = "Mascara Inicial de Subred:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "IP:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Requiero";
+            // 
+            // requiredBox
+            // 
+            this.requiredBox.FormattingEnabled = true;
+            this.requiredBox.Items.AddRange(new object[] {
+            "Subredes",
+            "Host por subred"});
+            this.requiredBox.Location = new System.Drawing.Point(206, 71);
+            this.requiredBox.Name = "requiredBox";
+            this.requiredBox.Size = new System.Drawing.Size(121, 21);
+            this.requiredBox.TabIndex = 4;
+            this.requiredBox.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // requiredNumberInput
+            // 
+            this.requiredNumberInput.Location = new System.Drawing.Point(99, 72);
+            this.requiredNumberInput.Name = "requiredNumberInput";
+            this.requiredNumberInput.Size = new System.Drawing.Size(85, 20);
+            this.requiredNumberInput.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(297, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(10, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = ".";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(401, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(10, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = ".";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(190, 31);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(10, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = ".";
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(313, 117);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(75, 23);
+            this.calcButton.TabIndex = 19;
+            this.calcButton.Text = "Calcular";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // rangosButton
+            // 
+            this.rangosButton.Location = new System.Drawing.Point(427, 115);
+            this.rangosButton.Name = "rangosButton";
+            this.rangosButton.Size = new System.Drawing.Size(75, 23);
+            this.rangosButton.TabIndex = 20;
+            this.rangosButton.Text = "Ver Rangos:";
+            this.rangosButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(189, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 20);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "/";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // oct1Input
+            // 
+            this.oct1Input.Location = new System.Drawing.Point(99, 31);
+            this.oct1Input.Name = "oct1Input";
+            this.oct1Input.Size = new System.Drawing.Size(85, 20);
+            this.oct1Input.TabIndex = 26;
+            // 
+            // oct4Input
+            // 
+            this.oct4Input.Location = new System.Drawing.Point(417, 31);
+            this.oct4Input.Name = "oct4Input";
+            this.oct4Input.Size = new System.Drawing.Size(85, 20);
+            this.oct4Input.TabIndex = 27;
+            // 
+            // oct3Input
+            // 
+            this.oct3Input.Location = new System.Drawing.Point(313, 31);
+            this.oct3Input.Name = "oct3Input";
+            this.oct3Input.Size = new System.Drawing.Size(85, 20);
+            this.oct3Input.TabIndex = 28;
+            // 
+            // oct2Input
+            // 
+            this.oct2Input.Location = new System.Drawing.Point(206, 31);
+            this.oct2Input.Name = "oct2Input";
+            this.oct2Input.Size = new System.Drawing.Size(85, 20);
+            this.oct2Input.TabIndex = 29;
+            this.oct2Input.ValueChanged += new System.EventHandler(this.numericUpDown4_ValueChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 168);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(475, 150);
+            this.dataGridView1.TabIndex = 30;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 339);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.oct2Input);
+            this.Controls.Add(this.oct3Input);
+            this.Controls.Add(this.oct4Input);
+            this.Controls.Add(this.oct1Input);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rangosButton);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.requiredNumberInput);
+            this.Controls.Add(this.requiredBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Mascara);
+            this.Controls.Add(this.slashBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.requiredNumberInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oct1Input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oct4Input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oct3Input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oct2Input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox slashBox;
+        private System.Windows.Forms.Label Mascara;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox requiredBox;
+        private System.Windows.Forms.NumericUpDown requiredNumberInput;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Button rangosButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown oct1Input;
+        private System.Windows.Forms.NumericUpDown oct4Input;
+        private System.Windows.Forms.NumericUpDown oct3Input;
+        private System.Windows.Forms.NumericUpDown oct2Input;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Subneteo/Subneteo/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Subneteo
+{
+    public partial class Form1 : Form
+    {
+        
+        private int oct1;
+        private int oct2;
+        private int oct3;
+        private int oct4;
+        private int requiredThing;
+        private int maskSlash;
+        private int requiredNumber;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void getData()
+        {
+            try
+            {
+
+                oct1 = Int32.Parse(oct1Input.Value.ToString());
+                oct2 = Int32.Parse(oct2Input.Value.ToString());
+                oct3 = Int32.Parse(this.oct3Input.Value.ToString());
+                oct4 = Int32.Parse(this.oct4Input.Value.ToString());
+                requiredNumber = Int32.Parse(this.requiredNumberInput.Value.ToString());
+                maskSlash = Int32.Parse(slashBox.SelectedItem.ToString());
+                requiredThing = Int32.Parse(requiredBox.SelectedIndex.ToString());
+                Console.WriteLine("Octeto1 " + oct1);
+                Console.WriteLine("Octeto2 " + oct2);
+                Console.WriteLine("Octeto3 " + oct3);
+                Console.WriteLine("Octeto4 " + oct4);
+                Console.WriteLine("Required Number" + requiredNumber);
+                Console.WriteLine("Mask Slash " + maskSlash);
+                Console.WriteLine("required thing index " + requiredThing);
+
+            }
+            catch (NullReferenceException e)
+            {
+                MessageBox.Show("Seleccione todos los campos solicitados");
+            }
+        }
+
+        bool validateData()
+        {
+            getData();
+            string errorMessage = "";
+            if (!(oct1 >= 0) || !(oct1 <= 255))
+            {
+                errorMessage += "Primer octeto de la IP no valido\n";
+            }
+
+            if (!(oct2 >= 0) || !(oct2 <= 255))
+            {
+                errorMessage += "Segundo octeto de la IP no valido\n";
+            }
+
+            if (!(oct1 >= 0) || !(oct1 <= 255))
+            {
+                errorMessage += "Tercer octeto de la IP no valido\n";
+            }
+
+            if (!(oct1 >= 0) || !(oct1 <= 255))
+            {
+                errorMessage += "Cuarto octeto de la IP no valido\n";
+            }
+
+            if (!(requiredNumber >= 0))
+            {
+                errorMessage += "No inserte datos negativos\n";
+            }
+
+            if (!errorMessage.Equals(""))
+            {
+                Console.WriteLine(errorMessage);
+                return false;
+            }
+            return true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            if (validateData())
+            {
+
+            }
+            
+        }
+
+        private void claseBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void oct3_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void oct1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown4_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
